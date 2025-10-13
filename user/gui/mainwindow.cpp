b98f8d339c3a7cbf3335e97be4571a69225586ee
carloszlopez <carloszepeda.contacto@gmail.com>
2025-10-12 23:41:19 -0600

    add user input configuration.

--- user/gui/mainwindow.cpp
+++ user/gui/mainwindow.cpp
@@ -3,6 +3,10 @@
 #include "dialogsettings.h"
 #include "ui_mainwindow.h"
 #include <QThread>
+#include <QSpinBox>
+#include <QComboBox>
+#include <QDialogButtonBox>
+
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -10,15 +14,48 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
     
-    /* Settings dialog */
-    connect(ui->actionConfiguration, &QAction::triggered, this, [this](){
-        DialogSettings* dlg = new DialogSettings(this);
-        dlg->setAttribute(Qt::WA_DeleteOnClose);
-        dlg->show();
-    });
-
     /* Create device */
     Device* dev = new Device("/dev/nxp_simtemp", "/sys/class/misc/nxp_simtemp");
+
+    /* Settings dialog */
+    connect(ui->actionConfiguration, &QAction::triggered, this, [this, dev]() {
+        DialogSettings* dlg = new DialogSettings(this);
+        dlg->setAttribute(Qt::WA_DeleteOnClose);
+
+        /* Read current device settings */
+        dev->read_attr("mode");
+        dev->read_attr("threshold_mC");
+        dev->read_attr("sampling_ms");
+
+        int mode = dev->get_attr("mode");
+        int threshold_mC = dev->get_attr("threshold_mC");
+        int sampling_ms = dev->get_attr("sampling_ms");
+
+        /* Show current device settings */
+        if (auto modeCombo = dlg->findChild<QComboBox*>("mode_comboBox"))
+            modeCombo->setCurrentIndex(mode);
+        if (auto thSpin = dlg->findChild<QSpinBox*>("threshold_spinBox"))
+            thSpin->setValue(threshold_mC);
+        if (auto sampSpin = dlg->findChild<QSpinBox*>("sampling_spinBox"))
+            sampSpin->setValue(sampling_ms);
+
+        /* Write user settings */
+        connect(dlg->findChild<QDialogButtonBox*>("buttonBox"), &QDialogButtonBox::accepted,
+                this, [dlg, dev]() {
+            /* Get user input */
+            int mode = dlg->findChild<QComboBox*>("mode_comboBox")->currentIndex();
+            int threshold_mC = dlg->findChild<QSpinBox*>("threshold_spinBox")->value();
+            int sampling_ms  = dlg->findChild<QSpinBox*>("sampling_spinBox")->value();
+
+            /* Write the attributes to the driver */
+            dev->write_attr("mode", std::to_string(mode));
+            dev->write_attr("threshold_mC", std::to_string(threshold_mC));
+            dev->write_attr("sampling_ms", std::to_string(sampling_ms));
+
+            dlg->accept();
+        });
+        dlg->show();
+    });
 
     /* Create new thread for reading */
     QThread* readThread = new QThread(this);
@@ -41,25 +78,6 @@ MainWindow::MainWindow(QWidget *parent)
         ui->th_checkBox->setChecked(sample.flags & EVENT_MASK_TH);
         /* Show time stamp in ns */
         ui->timestamp_ns_line->setText(QString::number(sample.timestamp_ns));
-
-        /* Read sysfs from driver */
-        dev->read_attr("mode");
-        dev->read_attr("threshold_mC");
-        dev->read_attr("sampling_ms");
-
-        /* Get sysfs values */
-        int mode = dev->get_attr("mode");
-        int threshold_mC = dev->get_attr("threshold_mC");
-        int sampling_ms = dev->get_attr("sampling_ms");
-
-        /* Show mode */
-        ui->norm_checkBox->setChecked(mode == 0);
-        ui->noisy_checkBox->setChecked(mode == 1);
-        ui->ramp_checkBox->setChecked(mode == 2);
-        /* Show threshold */
-        ui->threshold_mC_line->setText(QString::number(threshold_mC / 1000.0));
-        /* Show sampling */
-        ui->threshold_mC_line->setText(QString::number(sampling_ms));
     });
 
     /* Start reading thread */
